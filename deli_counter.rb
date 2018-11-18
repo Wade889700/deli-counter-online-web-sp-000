232deab7e944297b7b79cf69206ba9d7bdc5b35c
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 03:49:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def line(array)
 end
 
 def take_a_number(array,name)
-  index = array.index()
+  index = array.index(name).to_i + 1 
   my_array = ["Welcome, #{name}. You are number #{index} in line."]
   puts my_array.join
 end
