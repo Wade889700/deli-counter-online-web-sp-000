356fdaa9034c5f8b3c25c015146553cef1990c9b
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 03:52:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,8 @@ end
 
 def take_a_number(array,name)
   if array.size == 0
-    puts my_array = ["Welcome, #{name}. You are number 1 in line."].join
+    my_array = ["Welcome, #{name}. You are number 1 in line."]
+    puts my_array.join
   end
   index = array.index(name).to_i + 1
   my_array = ["Welcome, #{name}. You are number #{index} in line."]
