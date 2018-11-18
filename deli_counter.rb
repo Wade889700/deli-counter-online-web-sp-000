79e34500dea8593e05d007da5ae58afb0c3d79fe
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 03:24:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    counter = 0
+    my_array = []
+    start_line=["The line is currently: "]
+      while counter < array.size-1
+        a = counter + 1
+        my_array.push("#{a}. #{array[counter]}")
+        counter += 1
+      end
+  end
+end
+     
